8f16c24634f1e8dcaf520a7703d8446f8e3ee57a
chenlingjie <chenlingjie@cd121.com>
2017-11-14 15:48:55 +0800

    xg

--- app/baseModel.js
+++ app/baseModel.js
@@ -3,6 +3,8 @@ const config = require('../config');
 const localConst = require('./const');
 const localUtil = require('./util');
 const logger = require('./common/logger');
+const mailer = require('nodemailer');
+const emailConfig = config.email;
 const mysqlPool = mysql.createPool(config.mysql);
 
 module.exports = class BaseModel {
@@ -14,4 +16,18 @@ module.exports = class BaseModel {
     this.mysqlPool = mysqlPool;
     this.isDebug = config.server.debug;
   }
-}
+
+  sendMail(option) {
+    //防止timeout
+    let transporter = mailer.createTransport(emailConfig.senderAccount);
+    return new Promise((resolve, reject) => {
+      transporter.sendMail(option, (err, info) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(info);
+        }
+      });
+    });
+  }
+};
